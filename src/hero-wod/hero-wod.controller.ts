b68645d14ba2d40fb266a87e7b400ebf67aee2c1
jumyeong33 <jm01028250379@gmail.com>
2022-08-19 20:08:04 +0900

    [Modify] Use HeroWod Class method

--- src/hero-wod/hero-wod.controller.ts
+++ src/hero-wod/hero-wod.controller.ts
@@ -1,14 +1,23 @@
 import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
 import { HeroWodService } from './hero-wod.service';
+import { replyOk, replyErr } from 'src/utils/ReplyHelper';
+import { HeroWod } from './entities/hero-wod.entity';
 
 @Controller('hero-wod')
 export class HeroWodController {
   constructor(private readonly heroWodService: HeroWodService) {}
 
   @Get()
-  @HttpCode(200)
   async getAllHeroWod(): Promise<any> {
-    return this.heroWodService.getAll();
+    let heroWods: HeroWod[];
+
+    try {
+      heroWods = await this.heroWodService.getAll();
+    } catch (err) {
+      return replyErr(err);
+    }
+
+    return replyOk(heroWods.map((wod) => wod.serialize()));
   }
 
   @Get('create')
@@ -20,12 +29,28 @@ export class HeroWodController {
   @Post('random/filter')
   @HttpCode(200)
   async getRandomFilterHeroWod(@Body('id') wodIDs: [number]): Promise<any> {
-    return this.heroWodService.getRandomFilter(wodIDs);
+    let heroWod: HeroWod;
+
+    try {
+      heroWod = await this.heroWodService.getRandomFilter(wodIDs);
+    } catch (err) {
+      return replyErr(err);
+    }
+
+    return replyOk(heroWod.serialize());
   }
 
   @Get('random')
   @HttpCode(200)
   async getRandomHeroWod(): Promise<any> {
-    return this.heroWodService.getRandom();
+    let heroWod: HeroWod;
+
+    try {
+      heroWod = await this.heroWodService.getRandom();
+    } catch (err) {
+      return replyErr(err);
+    }
+
+    return replyOk(heroWod.serialize());
   }
 }
--- src/hero-wod/hero-wod.service.ts
+++ src/hero-wod/hero-wod.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateHeroWod } from './dto/createHeroWod';
 import { hero_wod } from '@prisma/client';
 import { CsvParser } from 'nest-csv-parser';
 import { createReadStream } from 'fs';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { HeroWod } from './entities/hero-wod.entity';
 
 @Injectable()
 export class HeroWodService {
@@ -12,9 +13,11 @@ export class HeroWodService {
     private readonly csvParser: CsvParser,
   ) {}
 
-  async getAll(): Promise<hero_wod[]> {
-    return await this.prismaService
+  async getAll(): Promise<HeroWod[]> {
+    const heroWods: [hero_wod] = await this.prismaService
       .$queryRaw`SELECT * from hero_wod ORDER BY 'desc'`;
+
+    return HeroWod.FromRows(heroWods);
   }
 
   async get(title: string): Promise<hero_wod> {
@@ -22,15 +25,22 @@ export class HeroWodService {
       .$queryRaw`SELECT * from hero_wod WHERE title=${title}`;
   }
 
-  async getRandom(): Promise<hero_wod[]> {
-    return await this.prismaService
+  async getRandom(): Promise<HeroWod> {
+    const heroWod: [hero_wod] = await this.prismaService
       .$queryRaw`SELECT * FROM hero_wod ORDER BY RAND() LIMIT 1`;
+
+    return HeroWod.FromRow(heroWod[0]);
   }
 
-  async getRandomFilter(wodIDs: [number]): Promise<any> {
+  async getRandomFilter(wodIDs: [number]): Promise<HeroWod> {
     const cond = wodIDs.toString();
-    return await this.prismaService
+    const heroWod: [hero_wod] = await this.prismaService
       .$queryRaw`SELECT * FROM hero_wod WHERE NOT FIND_IN_SET(id, ${cond}) ORDER BY RAND() LIMIT 1`;
+    if (heroWod.length < 1) {
+      throw new BadRequestException('You have Seen All wods from WodMon');
+    }
+
+    return HeroWod.FromRow(heroWod[0]);
   }
 
   async createByCsv() {
