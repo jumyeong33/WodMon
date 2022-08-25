13346a237b28d65a24106efc8a29d9b9fd87c604
jumyeong33 <jm01028250379@gmail.com>
2022-08-25 14:16:01 +0900

    [Delete] prismaMoudle delete

--- src/hero-wod/hero-wod.module.ts
+++ src/hero-wod/hero-wod.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CsvModule } from 'nest-csv-parser';
-import { PrismaMoudle } from 'src/prisma/prisma.module';
 import { HeroWodController } from './hero-wod.controller';
 import { HeroWodService } from './hero-wod.service';
 
 @Module({
-  imports: [PrismaMoudle, CsvModule],
+  imports: [CsvModule],
   controllers: [HeroWodController],
   providers: [HeroWodService],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { PrismaMoudle } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [PrismaMoudle],
+  exports: [UsersService],
   controllers: [UsersController],
   providers: [UsersService],
 })
